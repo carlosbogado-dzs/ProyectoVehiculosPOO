72ac5c410ce48cb23a026ad5a7bd9c87f92452ce
carlosbogado-dzs <carlos.bogado@fiuni.edu.py>
2026-04-20 00:11:39 -0300

    Update Main.java

--- Vehiculo/src/main/java/vehiculo/Main.java
+++ Vehiculo/src/main/java/vehiculo/Main.java
@@ -1,4 +1,7 @@
 package vehiculo;
+/**
+*Clase principal
+*/
 public class Main {
     public static void main(String[] args) {
         CentroControl centro = new CentroControl();
@@ -6,9 +9,11 @@ public class Main {
         Vehiculo dron = new DronTransporte("DRN-001");
         Vehiculo camion = new CaminonAutonomo("CAM-101");
 
+        //Regista unidades en el centro de control
         centro.registrarUnidad(dron);
         centro.registrarUnidad(camion);
-
+        
+        //monitorea la flota
         centro.monitorearFlota();
 
         //uso de interfaz
